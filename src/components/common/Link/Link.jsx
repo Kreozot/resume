f065fb5f82831adb173180c569a65bf4c0c2be85
Sergey Sharov <sssharov@domclick.ru>
2020-09-09 14:25:02 +0300

    Fixed GitHub icon in links, fixed sode smells

--- src/components/common/Link/Link.jsx
+++ src/components/common/Link/Link.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import InlineSvg from 'components/common/InlineSvg';
 
 import styles from './Link.module.scss';
+import GitHubIcon from 'images/icon-github.svg';
+
+const icons = {
+  github: GitHubIcon,
+};
 
 export default function Link(props) {
   const {
     url,
     title,
     icon,
+    type,
     className = '',
   } = props;
+
+  const Icon = useMemo(() => {
+    return icons[type] || null;
+  }, [type]);
 
   return (
     <div className={ `${ className } ${ styles.item }` }>
       <a className={ styles.link } href={ url } target="_blank" rel="noreferrer">
         <div className={ styles.icon }>
-          <InlineSvg content={ icon }/>
+          { Boolean(Icon)
+            ? <Icon/>
+            : <InlineSvg content={ icon }/>
+          }
         </div>
         <span className={ styles.title }>{ title }</span>
       </a>
